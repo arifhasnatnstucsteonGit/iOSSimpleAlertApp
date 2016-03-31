5b60779d466f9a3bbf1586c44285f8c1542cae92
arifhasnatnstucsteonGit <you@example.com>
2016-03-31 22:25:59 +0600

    simple alert

--- iOSsimpleAlert/ViewController.swift
+++ iOSsimpleAlert/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+   
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showMessage(sender: AnyObject) {
+        
+        let alert=UIAlertView()
+        
+        alert.title = "hey"
+        alert.message="This is an alert"
+        alert.addButtonWithTitle("working")
+        alert.show()
+    
     }
-
 
 }
 
